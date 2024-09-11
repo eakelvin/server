2050e2ac807c91628b682a70e81b4f0e587f4a7a
Kelvin Akaba <akabakelvin@gmail.com>
2024-09-11 14:26:29 +0000

    bugs fixed

--- src/models/subscribe.js
+++ src/models/subscribe.js
@@ -5,7 +5,7 @@ const subscribeSchema = new mongoose.Schema({
         type: String, 
         required: true, 
         unique: true
-    }
+    },
 }, { timestamps: true })
 
 const Subscribe = mongoose.model('Subscribe', subscribeSchema)
